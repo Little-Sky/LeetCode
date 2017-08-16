c5ea92da6acde9ebf5fa9e36fd73bed7471e29f2
Little-Sky <tianchicai@gmail.com>
2017-08-16 18:51:15 +0800

    Create 019. Remove Nth Node From End of List.cpp

---
+++ 019. Remove Nth Node From End of List.cpp
@@ -0,0 +1,32 @@
+// O(n) recursive
+// Faster than 15%
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getNthFromEnd (ListNode* head, int n) {
+        if (!head) {
+            return 0;
+        } else {
+            int dist = getNthFromEnd (head->next, n) + 1;
+            if (dist == n + 1) {
+                head->next = head->next->next;
+            }
+            return dist;
+        }
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (n == 0){
+            return head;
+        }
+        return (getNthFromEnd (head, n) == n ? head->next : head);
+    }
+};
