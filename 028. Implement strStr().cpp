7a08847efe1c45720ce593c70c26b03dd096cec8
Little-Sky <tianchicai@gmail.com>
2017-08-17 14:29:06 +0800

    Create 028. Implement strStr().cpp

---
+++ 028. Implement strStr().cpp
@@ -0,0 +1,10 @@
+// O() don't know, std function [Facepalm]
+// Faster than 28% - 28% [Facepalm]
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
