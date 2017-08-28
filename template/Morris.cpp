428ae9b53d8f9dbea7347a6a0e87c2bd290f5090
Little-Sky <tianchicai@gmail.com>
2017-08-28 09:50:59 +0800

    Update Morris.cpp

--- template/Morris.cpp
+++ template/Morris.cpp
@@ -26,3 +26,46 @@ while (cur) {
     cur = cur->right;
   }
 }
+
+// // O(n) iterative method
+// // Faster than 1% - 1%
+
+// // The idea is to view a tree as a group of parallel "anti-diagonal" direction chains, explore each of them in order
+// // When exploring, use the current pointer goes left, when reaches end, goes back, write down the number, and tries right
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         stack<TreeNode *> nodeStack;
+//         TreeNode * cur = root;
+//         while (cur || nodeStack.size()) {
+//             while (cur) {
+//                 nodeStack.push(cur);
+//                 cur = cur->left;
+//             }
+//             cur = nodeStack.top();
+//             nodeStack.pop();
+//             res.push_back(cur->val);
+//             cur = cur->right;
+//         }
+//         return res;
+//     }
+
+
+// // O(n) Trivial recursive solution
+// // Faster than 70%
+    
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         inorderTraversal_(root, res);
+//         return res;
+//     }
+// private:
+//     void inorderTraversal_(TreeNode* cur, vector<int> & res) {
+//         if (!cur) {
+//             return;
+//         }
+//         inorderTraversal_(cur->left, res);
+//         res.push_back(cur->val);
+//         inorderTraversal_(cur->right, res);
+//     }
